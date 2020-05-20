3468b3496f127b74ccd2294699e4a232c9370b67
Margret `Pax` Williams <pax.margret@gmail.com>
2020-05-19 18:46:18 -0700

    :sparkles: Print number of tasks

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -111,7 +111,7 @@ class _TasksScreenState extends State<TasksScreen> {
                   ),
                 ),
                 Text(
-                  '12 Tasks',
+                  '${tasks.length} Task${tasks.length == 1 ? "" : "s"}',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 18,
