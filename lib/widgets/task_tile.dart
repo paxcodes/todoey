1a83ab5a63d894a81fe4a031df3ae4a2cf6cb1cd
Margret `Pax` Williams <pax.margret@gmail.com>
2020-05-19 17:30:15 -0700

    :sparkles:👩🏻‍💻 Implement a list builder
    👩🏻‍💻 I passed the whole `Task` object instead of individual props.

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,52 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:todoeyflutter/models/task.dart';
 
-class TaskTile extends StatefulWidget {
-  final String taskName;
-
-  TaskTile(this.taskName);
-
-  @override
-  _TaskTileState createState() => _TaskTileState();
-}
-
-class _TaskTileState extends State<TaskTile> {
-  bool isCompleted = false;
+class TaskTile extends StatelessWidget {
+  final Task task;
+  final Function onChanged;
+  TaskTile(this.task, {this.onChanged});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       contentPadding: EdgeInsets.zero,
       title: Text(
-        widget.taskName,
+        task.name,
         style: TextStyle(
           fontSize: 20,
           decoration:
-              isCompleted ? TextDecoration.lineThrough : TextDecoration.none,
+              task.isDone ? TextDecoration.lineThrough : TextDecoration.none,
         ),
       ),
-      trailing:
-          TaskCheckbox(isChecked: isCompleted, onChanged: _setCompletedStatus),
-    );
-  }
-
-  void _setCompletedStatus(bool newValue) {
-    setState(() {
-      isCompleted = newValue;
-    });
-  }
-}
-
-class TaskCheckbox extends StatelessWidget {
-  TaskCheckbox({this.isChecked, this.onChanged});
-  final bool isChecked;
-  final Function onChanged;
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      value: isChecked,
-      activeColor: Colors.lightBlueAccent,
-      onChanged: onChanged,
+      trailing: Checkbox(
+        value: task.isDone,
+        activeColor: Colors.lightBlueAccent,
+        onChanged: onChanged,
+      ),
     );
   }
 }
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:todoeyflutter/widgets/task_tile.dart';
+import 'package:todoeyflutter/models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: "Buy milk"),
+    Task(name: "Buy eggs"),
+    Task(name: "Buy bread"),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile("Buy milk"),
-        TaskTile("Buy detergent"),
-        TaskTile("Buy bananas"),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) =>
+          TaskTile(tasks[index], onChanged: (bool newValue) {
+        setState(() {
+          tasks[index].toggleDone();
+        });
+      }),
+      itemCount: tasks.length,
     );
   }
 }
