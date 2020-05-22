e98e0bf40b6a0b21e40646f6871b341a85de5bd0
Margret `Pax` Williams <pax.margret@gmail.com>
2020-05-22 06:50:14 -0700

    :sparkles:👩🏻‍💻 Use ChangeNotifierProvider to print num of tasks and list of tasks

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:todoeyflutter/models/task.dart';
+import 'package:provider/provider.dart';
+import 'package:todoeyflutter/widgets/task_tile.dart';
 
 class TasksList extends StatelessWidget {
-  TasksList({this.tasks, this.taskBuilder});
-
-  final List<Task> tasks;
-  final Function taskBuilder;
-
   @override
   Widget build(BuildContext context) {
+    final List<Task> tasks = context.watch<ListOfTasks>().tasks;
+
     return ListView.builder(
-      itemBuilder: taskBuilder,
+      itemBuilder: (context, index) => TaskTile(
+        tasks[index],
+        onChanged: (bool newValue) {
+          context.read<ListOfTasks>().toggleDone(index);
+        },
+      ),
       itemCount: tasks.length,
     );
   }
